ecb1893b8f7198853554e54bcd8c7519588fb8ff
arthurbackes <arthur.backes@student.uclouvain.be>
2024-12-16 12:29:13 +0100

    recherche dans les listes partagées

--- routes/all-lists.js
+++ routes/all-lists.js
@@ -5,9 +5,22 @@ const List = require("../models/list");
 router.get("/", async (req, res) => {
     try {
         const lists = await List.find({});
-        res.render("lists/all-lists", { lists: lists});
+        res.render("lists/all-lists", { lists: lists, results: false});
     } catch {
         res.redirect("/");
+    }
+})
+
+router.post("/search", async (req, res) => {
+    const nameQuery = req.body.name || '';
+    try {
+        const lists = await List.find({});
+        const results = await List.find({name: { $regex: `^${nameQuery}`, $options: 'i' }});
+        console.log(results, req.body.name)
+        res.render('lists/all-lists', { lists, results }); 
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erreur serveur lors de la recherche');
     }
 })
 module.exports = router;
